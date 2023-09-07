1993310849c2d75bab0aea42e83d8a181322ec11
Sebastian Ciancio <sebastian.ciancio@gmail.com>
2023-09-07 15:43:02 -0300

    Creación de la estructura para el Login

---
+++ app/src/main/java/ar/edu/utn/frba/placesify/ui/theme/login/ui/LoginScreen.kt
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.placesify.ui.theme.login.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun  LoginScreen(){
+
+}
---
+++ app/src/main/java/ar/edu/utn/frba/placesify/ui/theme/login/ui/LoginViewModel.kt
@@ -0,0 +1,2 @@
+package ar.edu.utn.frba.placesify.ui.theme.login.ui
+
