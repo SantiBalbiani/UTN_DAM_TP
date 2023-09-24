5dc1b7de202aab724b6e614b0a86710a27a59cb1
gabycosta04 <ccosta@frba.utn.edu.ar>
2023-09-24 17:14:49 -0300

    modifico algo del main activity para que funcione con lo cambiado

--- app/src/main/java/ar/edu/utn/frba/placesify/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/placesify/MainActivity.kt
@@ -145,6 +145,7 @@ class MainActivity : ComponentActivity() {
                         composable("detail_places") {
                             DetailPlacesScreen(
                                 DetailPlacesViewModel(),
+                                FavoritiesViewModel(BackendService.instance),
                                 navController = navController
                             )
                         }
